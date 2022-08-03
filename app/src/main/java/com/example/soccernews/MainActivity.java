702482db245660f5adf2a90b99412d8acec38734
allan8araujo <allanzao08@gmail.com>
2022-08-02 21:27:13 -0300

    new [setting_up_layout] layout add

--- app/src/main/java/com/example/soccernews/MainActivity.java
+++ app/src/main/java/com/example/soccernews/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.soccernews;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.soccernews.databinding.ActivityMainBinding;
 
@@ -15,5 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_news, R.id.navigation_dashboard).build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 }
